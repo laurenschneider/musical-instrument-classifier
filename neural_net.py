594ca9aff26d2dbec29e26840dfad984b3be3b84
Lauren Braun <lbraun21@gmail.com>
2019-04-30 18:22:54 -0700

    add requirements

---
+++ neural_net.py
@@ -0,0 +1,16 @@
+"""
+Neural network
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+# TODO: get dataset
+
+# TODO:
+# build model
+# set up layers
+
+# TODO: train model
+
+# TODO: evaluate accuracy
