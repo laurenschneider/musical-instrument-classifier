1ed21e9012ee5294affd856ec3278190e486160c
Lauren Braun <lbraun21@gmail.com>
2019-04-30 21:11:58 -0700

    use tensorflow_datasets api for nsynth data

--- neural_net.py
+++ neural_net.py
@@ -4,8 +4,11 @@ Neural network
 
 import tensorflow as tf
 from tensorflow import keras
+import tensorflow_datasets as tfds
 
 # TODO: get dataset
+
+data = tfds.load("nsynth")
 
 # TODO:
 # build model
