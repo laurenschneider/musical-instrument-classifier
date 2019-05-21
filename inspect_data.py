2d940b01106e976b913309a59fa09a058f24abf6
Lauren Braun <lbraun21@gmail.com>
2019-05-21 14:00:19 -0700

    add FFT plot and time/amplitude plot

--- inspect_data.py
+++ inspect_data.py
@@ -5,6 +5,7 @@ Data must be downloaded from nsynth website and placed in data dir
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
 import pathlib
 
 
@@ -20,18 +21,43 @@ print(instrument[0])    # print first part of filename to see instrument
 
 example_path_str = str(TEST_AUDIO_PATH/test_acoustic[0])
 
+"""
+Initial read and inspect data in time domain
+"""
+
 # get time series and sample rate
 y, sample_rate = librosa.load(example_path_str)
 
-print("y: ", y)
-print("sample rate: ", sample_rate)
+# plot amplitude in time domain
+plt.figure()
+librosa.display.waveplot(y, sr=sample_rate)
+plt.show()
+
+"""
+Fast Fourier Transform to convert to the frequency domain
+"""
+
+yf = np.fft.fft(y)  
+N = len(y)  # number of samples
+reals = int(N/2)    # only need real valued data, so take first half
+yf =  yf[0:reals]
+
+# plot FFT
+plt.figure(1, figsize=(8,6))
+plt.plot(yf, color='orange')
+plt.xlabel('?') # TODO: what should this label be?
+plt.ylabel('Amplitude')
+plt.show()
+
+"""
+MFCCs
+"""
 
 mfccs = librosa.feature.mfcc(y=y, sr=sample_rate)
-print(mfccs)
 
 # visualize mfcc
 plt.figure(figsize=(10,4))
 librosa.display.specshow(mfccs, x_axis='time')
 plt.colorbar()
 plt.title('instrument[0]')
-plt.show()  # TODO: save plots in separate dir 
+plt.show()  # TODO: save plots in separate dir
