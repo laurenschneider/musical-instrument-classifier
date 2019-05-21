d7b71fdda263c08bba7835fbe47e8b20a6315cee
Lauren Braun <lbraun21@gmail.com>
2019-05-21 16:37:14 -0700

    add spectograms with decomposition

--- inspect_data.py
+++ inspect_data.py
@@ -16,10 +16,10 @@ TEST_AUDIO_PATH = DATA_PATH/'nsynth-test/audio'
 test_acoustic = [file.name for file in TEST_AUDIO_PATH.iterdir()
                     if 'acoustic' in file.name]
 
-instrument = test_acoustic[0].split('_')
+instrument = test_acoustic[1200].split('_')
 print(instrument[0])    # print first part of filename to see instrument
 
-example_path_str = str(TEST_AUDIO_PATH/test_acoustic[0])
+example_path_str = str(TEST_AUDIO_PATH/test_acoustic[1200])
 
 """
 Initial read and inspect data in time domain
@@ -27,12 +27,12 @@ Initial read and inspect data in time domain
 
 # get time series and sample rate
 y, sample_rate = librosa.load(example_path_str)
-
+"""
 # plot amplitude in time domain
 plt.figure()
 librosa.display.waveplot(y, sr=sample_rate)
 plt.show()
-
+"""
 """
 Fast Fourier Transform to convert to the frequency domain
 """
@@ -41,32 +41,55 @@ yf = np.fft.fft(y)
 N = len(y)  # number of samples
 reals = int(N/2)    # only need real valued data, so take first half
 yf =  yf[0:reals]
-
+"""
 # plot FFT
 plt.figure(1, figsize=(8,6))
 plt.plot(yf, color='orange')
 plt.xlabel('?') # TODO: what should this label be?
 plt.ylabel('Amplitude')
 plt.show()
-
+"""
 """
 MFCCs
 """
 
 mfccs = librosa.feature.mfcc(y=y, sr=sample_rate)
-
+"""
 # visualize mfcc
 plt.figure(figsize=(10,4))
 librosa.display.specshow(mfccs, x_axis='time')
 plt.colorbar()
 plt.title('instrument[0]')
 plt.show()  # TODO: save plots in separate dir
+"""
+
 
 """
 Decomposition
 """
 # TODO: quick experiment to compare spectogram, harmonic, and percussive of a few
-#       different instrument examples 
+#       different instrument examples
 
 stft = librosa.stft(y)    # apply short time fourier transform
 harmonic, percuss = librosa.decompose.hpss(stft)  # get harmonic and percussive components
+plt.figure()
+
+plt.subplot(3,1,1)
+librosa.display.specshow(librosa.amplitude_to_db(np.abs(stft),
+ref=np.max), y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Full Power Spectogram')
+
+plt.subplot(3,1,2)
+librosa.display.specshow(librosa.amplitude_to_db(np.abs(harmonic),
+                                                 ref=np.max), y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Harmonic Power Spectogram')
+
+plt.subplot(3,1,3)
+librosa.display.specshow(librosa.amplitude_to_db(np.abs(percuss),
+                                                 ref=np.max), y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Percussive Power Spectogram')
+#plt.show()
+plt.savefig('plots/' + instrument[0] +'.png')
