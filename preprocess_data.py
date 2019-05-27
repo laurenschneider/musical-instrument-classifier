fe5dac768b1081750164c2962895caf561ad3aa4
Lauren Braun <lbraun21@gmail.com>
2019-05-27 10:25:03 -0700

    generate flat data files. currently not the same length, need to figure out feature file

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,7 @@
 
 import librosa
 import pathlib
-
+import numpy as np
 
 DATA_PATH = pathlib.Path('data')
 TEST_AUDIO_PATH = DATA_PATH/'nsynth-test/audio'
@@ -15,8 +15,10 @@ test_acoustic = [file.name for file in TEST_AUDIO_PATH.iterdir()
 features = np.array([])
 labels = np.array([])
 
+counter = 0
+
 for wavfile in test_acoustic:
-    y, sample_rate = librosa.load(wavfile)
+    y, sample_rate = librosa.load(TEST_AUDIO_PATH/wavfile)
 
     # decompose to get harmonic and percussive features
     # returns numpy 2-d complex array
@@ -26,8 +28,22 @@ for wavfile in test_acoustic:
     # get mfccs as another feature
     # numpy 2-d float array
     mfccs = librosa.feature.mfcc(y=y, sr=sample_rate, n_mfcc=30)
-    features = np.append(mfccs)
+    features = np.append(features, [mfccs])     # TODO: append is wrong
 
     instrument = wavfile.split('_')     # returns a list
     instr_name = instrument[0]          # first index is the name string
-    labels = np.append(instr_name)
+    labels = np.append(labels, [instr_name])
+
+    if counter%100 == 0:
+        print(counter, "files written")
+    counter = counter + 1
+
+
+# for testing
+# write arrays to files to inspect
+TEST_PATH = pathlib.Path('tests')
+feature_file = TEST_PATH/'features.txt'
+label_file = TEST_PATH/'labels.txt'
+
+np.savetxt(feature_file, features, fmt="%s")
+np.savetxt(label_file, labels, fmt="%s")
