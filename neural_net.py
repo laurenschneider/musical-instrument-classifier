ea07b0f50d5ac1fa2fee16ced5d20dc242ae4740
tacemonster <40149698+tacemonster@users.noreply.github.com>
2019-05-28 16:10:59 -0700

    Neural net reaches ~50% acc in 50 epochs with test data

--- neural_net.py
+++ neural_net.py
@@ -8,25 +8,32 @@ import numpy as np
 
 # get data from preprocess_data.py
 features = np.loadtxt('tests/features.txt')
-print(features.shape)
+# print(features.shape)
 labels = np.loadtxt('tests/labels.txt')
-print(labels.shape)
+# print(labels.shape)
+dictfile = open('tests/dictionary.txt', 'r')
+instruments = eval(dictfile.read())
+category_count = len(instruments)
 
+label_cats = keras.utils.to_categorical(labels, num_classes=category_count)
+print(label_cats.shape)
 # build model
 model = keras.models.Sequential()
 
 # set up layers
-# TODO: needs input shape
-model.add(keras.layers.Dense(100, input_shape=(5190,)))
-model.add(keras.layers.Activation('relu'))  # can experiment with different activation functions
-model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+model.add(keras.layers.Dense(50, input_dim=5190))
+model.add(keras.layers.Activation('sigmoid'))
+#model.add(keras.layers.Activation('relu'))  # can experiment with different activation functions
+model.add(keras.layers.Dense(100))
+model.add(keras.layers.Activation('sigmoid'))
+model.add(keras.layers.Dense(category_count, activation=tf.nn.sigmoid))
 
 model.compile(optimizer='rmsprop',
-              loss='sparse_categorical_crossentropy',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # TODO: train model
-model.fit(x=features, y=labels, epochs=20)
+model.fit(x=features, y=label_cats, epochs=50)
 
 # TODO: create testing dataset
 # TODO: evaluate accuracy
