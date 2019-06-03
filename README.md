11bb495ac1a333584b1e7d24a4d8b8f760c33d80
Lauren Braun <lbraun21@gmail.com>
2019-06-03 14:49:24 -0600

    add logic to ignore bass and organ

--- README.md
+++ README.md
@@ -6,21 +6,26 @@ Lauren Schneider <br> <br>
 
 Final project for Computers, Sound, & Music at Portland State University.
 
-###Project Setup
+### Project Setup
+<b>Windows: </b>
 This project should be run in an Anaconda virtual environment.
 Once inside the virtual environment, run the following commands:
-
-```conda install tensorflow```
 
 ```conda install -c conda-forge librosa```
 
 ```pip install -r requirements.txt```
 
-This should get the environment set up properly for running the code
+<b>Mac OS/Linux:</b>
+```pip install librosa```
+
+```pip install -r requirements.txt```
+
+This should get the environment set up properly for running the code.
 
 
-###Data Preprocessing
+### Data Preprocessing
 
-Should you want to re-run the preprocessing, you will need to download the nsynth dataset
+The NSynth dataset contains over 300,000 WAV audio files.
+Should you wish to re-run the preprocessing, you will need to download the NSynth dataset
 and put the files into the workspace. We chose to handle the files manually because accessing
-the dataset through tensorflow was too time-heavy for our machines.
+this particular dataset through TensorFlow's dataset API requires more processing power than we had available.
--- preprocess_data.py
+++ preprocess_data.py
@@ -8,6 +8,10 @@ import numpy as np
 
 
 def get_files(test_or_train):
+    """
+    Select wavfiles to preprocess
+    :param test_or_train: String
+    """
     data_path = pathlib.Path('data')
     folder = 'nsynth-' + test_or_train + '/audio'
     audio_path = data_path/folder
@@ -16,10 +20,22 @@ def get_files(test_or_train):
     acoustic = [file.name for file in audio_path.iterdir()
                     if 'acoustic' in file.name]
 
-    test_files(acoustic, audio_path, test_or_train)
+    if test_or_train == "train":
+        # remove bass and organ from train set
+        acoustic = [file.name for file in audio_path.iterdir()
+                        if 'acoustic' in file.name and 'bass' not in file.name
+                        and 'organ' not in file.name]
+
+    extract_data(acoustic, audio_path, test_or_train)
 
 
-def test_files(file_names, audio_path, test_or_train):
+def extract_data(file_names, audio_path, test_or_train):
+    """
+    Get feature data from wavfiles
+    :param file_names: List of Strings
+    :param audio_path: String
+    :param test_or_train: String
+    """
     features = []
     labels = np.array([])
 
@@ -40,12 +56,6 @@ def test_files(file_names, audio_path, test_or_train):
         instrument = wavfile.split('_')     # returns a list
         instr_name = instrument[0]          # first index is the name string
 
-        # add to dictionary if missing
-        """
-        if instr_name not in instrument_values:
-            instrument_values[instr_name] = len(instrument_values) // 2
-            instrument_values[str(instrument_values[instr_name])] = instr_name
-        """
         # add correct value to labels that corresponds to dict entry for instrument key
         labels = np.append(labels, dictionary[instr_name])
 
@@ -57,6 +67,12 @@ def test_files(file_names, audio_path, test_or_train):
 
 
 def write_files(features, labels, test_or_train):
+    """
+    Writes feature data and corresponding label to keras friendly file
+    :param features: numpy array of MFCCs for each instrument sound
+    :param labels: String
+    :param test_or_train: String
+    """
 
     # write arrays to files to inspect
     if test_or_train == 'test':
@@ -66,16 +82,10 @@ def write_files(features, labels, test_or_train):
 
     feature_file = file_path/'features.txt'
     label_file = file_path/'labels.txt'
-    dictionary = file_path/'dictionary.txt'
 
     np.savetxt(feature_file, np.array(features), fmt="%s")
     np.savetxt(label_file, labels, fmt="%s")
-    """
-    dictfile = open(dictionary,"w")
-    dictfile.write(str(instrument_values))
-    dictfile.close()
-    """
 
 
-#get_files('train')
-get_files('test')
+get_files('train')
+#get_files('test')
