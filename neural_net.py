888b46ddbe9ecffa04984629935fd3b86462c7ed
Lauren Braun <lbraun21@gmail.com>
2019-06-06 05:37:24 -0600

    refactor to make code more modular. need to add some comments in class methods

--- neural_net.py
+++ neural_net.py
@@ -5,85 +5,83 @@ Must have run preprocess_data.py to generate keras friendly data files
 before running this neural net.
 """
 
+import time
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import time
 from sklearn.metrics import confusion_matrix
 import seaborn
 from matplotlib import pyplot
 
+class Neural_Net:
 
-def load_data(filepath):
-    """
-    Load preprocessed data to run through keras model.
-    :param filepath: String
-    :returns: numpy array
-    """
-    starttime = time.time()
-    # get data from preprocessed files
-    data = np.loadtxt(filepath)
-    endtime = time.time() - starttime
-    print('loaded data in ', endtime, 's.')
-    
-    return data
+    def __init__(self):
+        self.model = keras.models.Sequential()
+
+    def build_model(self, category_count):
+        """
+        Create keras neural network model
+        :param category_count: int
+        """
+        # set up layers
+        self.model.add(keras.layers.Dense(50, input_dim=30))
+        self.model.add(keras.layers.Activation('sigmoid'))
+        self.model.add(keras.layers.Dense(100))
+        self.model.add(keras.layers.Activation('sigmoid'))
+        self.model.add(keras.layers.Dense(category_count, activation=tf.nn.sigmoid))
+
+        self.model.compile(optimizer='rmsprop',
+                          loss='categorical_crossentropy',
+                          metrics=['accuracy'])
+
+    def train(self, train_features, train_label_cats):
+        """
+
+        """
+        checkpointer = keras.callbacks.ModelCheckpoint(filepath='/tmp/weights.hdf5',
+                                                        verbose=1,
+                                                        save_best_only=True)
+        starttime = time.time()
+        self.model.fit(x=train_features,
+                        y=train_label_cats,
+                        epochs=10,
+                        callbacks=[checkpointer])
+        endtime = time.time() - starttime
+        print('one epoch in  ', endtime, 's.')
 
 
-# get data
 
-train_labels = load_data('train/labels.txt')
-train_features = load_data('train/features.txt')
-test_labels = load_data('test/labels.txt')
-test_features = load_data('test/features.txt')
+    def predict(self, test_features, test_label_cats):
+        """
 
+        """
+        # create testing dataset
+        self.model.evaluate(test_features, test_label_cats)
 
-dictfile = open('dictionary.txt', 'r')
-instr_dict = eval(dictfile.read())
-category_count = len(instr_dict)
-train_label_cats = keras.utils.to_categorical(train_labels, num_classes=category_count)
-test_label_cats = keras.utils.to_categorical(test_labels, num_classes=category_count)
+        # evaluate accuracy
+        predictions = self.model.predict(test_features)
+        flat_predictions = np.argmax(predictions, axis=1)
+        print(flat_predictions)
+        print(predictions.shape)
+        flat_actuals = np.argmax(test_label_cats, axis=1)
+        print(flat_actuals)
+        conf_matrix = confusion_matrix(flat_actuals, flat_predictions)
+        print(conf_matrix.shape)
 
-# build model
+        return conf_matrix
 
-model = keras.models.Sequential()
+    def plot_heatmap(self, conf_matrix, labels):
+        """
 
-# set up layers
-model.add(keras.layers.Dense(50, input_dim=30))
-model.add(keras.layers.Activation('sigmoid'))
-model.add(keras.layers.Dense(100))
-model.add(keras.layers.Activation('sigmoid'))
-model.add(keras.layers.Dense(category_count, activation=tf.nn.sigmoid))
-
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-# train model
-
-# use checkpointer to hold weights to prevent overtraining
-checkpointer = keras.callbacks.ModelCheckpoint(filepath='/tmp/weights.hdf5', verbose=1, save_best_only=True)
-starttime = time.time()
-model.fit(x=train_features, y=train_label_cats, epochs=10, callbacks=[checkpointer])
-endtime = time.time() - starttime
-print('one epoch in  ', endtime, 's.')
-
-# create testing dataset
-model.evaluate(test_features, test_label_cats)
-
-# evaluate accuracy
-predictions = model.predict(test_features)
-flat_predictions = np.argmax(predictions, axis=1)
-print(flat_predictions)
-print(predictions.shape)
-flat_actuals = np.argmax(test_label_cats, axis=1)
-print(flat_actuals)
-conf_matrix = confusion_matrix(flat_actuals, flat_predictions)
-print(conf_matrix.shape)
-
-all_labels = instr_dict.keys()
-seaborn.heatmap(conf_matrix, cmap='Blues', annot=True, fmt='d', xticklabels=all_labels, yticklabels=all_labels)
-pyplot.xlabel('Actual')
-pyplot.ylabel('Predicted')
-pyplot.title('Classification of Instruments from wav files')
-pyplot.savefig('matrix.png')
-pyplot.show()
+        """
+        seaborn.heatmap(conf_matrix,
+                        cmap='Blues',
+                        annot=True,
+                        fmt='d',
+                        xticklabels=labels,
+                        yticklabels=labels)
+        pyplot.xlabel('Actual')
+        pyplot.ylabel('Predicted')
+        pyplot.title('Classification of Instruments from wav files')
+        pyplot.savefig('matrix.png')
+        pyplot.show()
